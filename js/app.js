5f0b5d53f4ef1fd93bd98088950850c4ff3681c5
unknown <evarizm32@gmail.com>
2025-03-04 03:27:42 +0200

    map:

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const topic = "image";
 
 // Map Tile Layers
 const lightTileLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     maxZoom: 20
 });
 
